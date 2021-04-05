301e3053875d0a7f57df5c429ca92e3ba098b43a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2021-04-05 16:38:16 +0400

    Revert "Revert "reset sessions""
    This reverts commit 2dd5bc232674705020027a23425e2c048ac629ef.

--- backend/src/ru/bia_tech/statsbit/utils/agent_id.clj
+++ backend/src/ru/bia_tech/statsbit/utils/agent_id.clj
@@ -17,10 +17,10 @@
    [clojure.string :as str]))
 
 (defn build [app-id server-id]
-  (str/join ":" ["v1" app-id server-id]))
+  (str/join ":" ["v2" app-id server-id]))
 
 (defn parse [value]
-  (let [[found? app-id server-id] (re-find #"v1:(\d+):(\d+)" value)]
+  (let [[found? app-id server-id] (re-find #"v2:(\d+):(\d+)" value)]
     (when-not found?
       (throw (ex-info "Wrong agent-id" {:type :wrong-agent-id, :value value})))
     [(Integer/parseInt app-id)
